9dce62c88b5cd7d1d3a1cdf1f04cd0e91dc978a8
Thuong <truonghoaithuong2000@gmail.com>
2018-06-04 20:36:18 +0700

    refactoring code or has higher precedence than and

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -2,22 +2,22 @@ class V1::SessionsController < V1::ApiController
   before_action :require_login, only: :destroy
   
   def create
-    authenticate_token and
-    head :created or (
-      @user = User.find_by_email(params[:email]) and
-      @user.valid_password?(params[:password]) and
-      @user.jwt_matcher = rand(1000000).to_s and
-      @user.save and
-      begin
-        payload = { user_id: @user.id, jwt_matcher: @user.jwt_matcher }
-        jwt = JWT.encode(
-          { data: payload, exp: (Time.now + 2.days).to_i },
-          Rails.application.secrets.secret_key_base,
-          'HS256'
-        )
-        render :create, status: :created, locals: { token: jwt }
-      end) or
-    head :unauthorized
+    head :unauthorized unless
+    @user = User.find_by_email(params[:email]) and
+    @user.valid_password?(params[:password]) and
+    @user.jwt_matcher = rand(1000000).to_s and
+    @user.save and
+    begin
+      payload = { user_id: @user.id, jwt_matcher: @user.jwt_matcher }
+      jwt = JWT.encode(
+        { data: payload, exp: (Time.now + 2.days).to_i },
+        Rails.application.secrets.secret_key_base,
+        'HS256'
+      )
+      render :create, status: :created, locals: { token: jwt }
+    end unless
+    authenticate_token and head :created
+
   end
   
   def destroy
